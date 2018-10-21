fb7277b04e453bfecfab80dc8da03a0871e111ee
zhangzhuo05 <zhangzhuo05@baidu.com>
2018-10-21 17:53:40 +0800

    添加HelloWorld

---
+++ javase01/src/com/bd/javabase/FinalTest.java
@@ -0,0 +1,16 @@
+package com.bd.javabase;
+
+import java.util.Scanner;
+
+public class FinalTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		final int NUM = 3;
+		System.out.print("输入会员卡号：");
+		Scanner sc =  new Scanner(System.in);
+		int i = sc.nextInt();
+		System.out.println(i);
+	}
+
+}
