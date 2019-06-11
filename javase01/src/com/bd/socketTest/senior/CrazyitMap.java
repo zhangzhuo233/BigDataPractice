8a5b1d9d94bccd5096aa7822523452f1a4c2f0a6
zhangzhuo05 <zhangzhuo05@baidu.com>
2019-06-11 11:10:36 +0800

    add crazymap

---
+++ javase01/src/com/bd/socketTest/senior/CrazyitMap.java
@@ -0,0 +1,10 @@
+package com.bd.socketTest.senior;
+
+/**
+ * @Author: zhangzhuo05
+ * @Date: 2019-05-21 19:51
+ * @Description: 组合HashMap对象来实现CrazyitMap，要求value不能重复
+ */
+public class CrazyitMap<K,V> {
+
+}
